3f9221b2faba73d61ebf15650028c1cba31c651d
TitidTerbang <yudisbaek@gmail.com>
2024-10-31 12:54:18 +0700

    tugas 2

---
+++ modul3/demo/tugas2/index.php
@@ -0,0 +1,27 @@
+<?php
+
+function cetakBilangan(int $n): void {
+    if ($n <= 0) {
+        echo "Input harus bilangan bulat positif.\n";
+        return;
+    }
+
+    for ($i = 1; $i <= $n; $i++) {
+        if ($i % 4 === 0 && $i % 6 === 0) {
+            echo "Pemrograman Website 2024\n";
+        } elseif ($i % 5 === 0) {
+            echo "2024\n";
+        } elseif ($i % 4 === 0 && $i % 6 !== 0) {
+            echo "Pemrograman\n";
+        } elseif ($i % 6 === 0 && $i % 4 !== 0) {
+            echo "Website\n";
+        } else {
+            echo $i . "\n";
+        }
+    }
+}
+
+
+cetakBilangan(50);
+
+?>
